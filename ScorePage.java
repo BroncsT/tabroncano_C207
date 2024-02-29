74b67acf8a3faa2aac3b09b7907456c535d57154
BroncsT <161276107+BroncsT@users.noreply.github.com>
2024-02-29 23:27:23 +0800

    Add files via upload

---
+++ ScorePage.java
@@ -0,0 +1,20 @@
+package com.example.broncano;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.broncano.StoreScore;
+
+public class ScorePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_page);
+        TextView tv = findViewById(R.id.score);
+        StoreScore ss = new StoreScore();
+        tv.setText(Integer.toString(StoreScore.score));
+    }
+}
